dcba6dc82906c6a94985db340bdb7a7ece4b4f06
mhk98 <mohsinkabirseo@gmail.com>
2024-07-24 22:47:51 +0600

    latest

--- Components/Layout/Header/Header.js
+++ Components/Layout/Header/Header.js
@@ -54,7 +54,7 @@ const Header = () => {
             <small>+8801601872740</small>
           </div>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-2">
           <a href="#" className="text-white hover:text-gray-400">
             <FaFacebookF />
           </a>
